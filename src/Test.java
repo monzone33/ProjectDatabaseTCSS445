91c501bf8e225a4b8b58aed6f9fa56df85dce3bd
Bao <thebaoking@gmail.com>
2022-06-03 23:30:14 -0700

    test

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Change to master");
+        System.out.println("Change to master from branch");
     }
 }
