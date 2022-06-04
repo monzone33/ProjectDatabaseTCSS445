9760760e618127c2e8bacad034210d8c42fff839
Emanuel Monzon <emanuel-monzon97@outlook.com>
2022-06-04 01:01:52 -0700

    Added SQL connection

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,36 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Made changes to master...");
+        String url = "jdbc:mysql://localhost:3306/project";
+
+        //if you created a separate account, use that, otherwise use root
+        String user = "root";
+        String password = "123456";
+
+        //quickly grab everything from Customers table and display to show everything worked
+        String query = "SELECT * FROM Lore";
+
+        try (Connection con = DriverManager.getConnection(url, user, password);
+             Statement st = con.createStatement();        // Allows us to do things such as execute things.
+             ResultSet rs = st.executeQuery(query)) {
+
+            while (rs.next()) {
+
+                System.out.println(rs.getString(1) + " " + rs.getString(2) + " " + rs.getString(3) + " " + rs.getString(4));
+            }
+
+        } catch (SQLException ex) {
+
+            Logger lgr = Logger.getLogger(Test.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+        }
     }
 }
