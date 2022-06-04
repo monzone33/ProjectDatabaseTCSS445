798fd8a327e3527d3589911ac3d8ada2b7910ea3
Emanuel Monzon <emanuel-monzon97@outlook.com>
2022-06-04 00:04:09 -0700

    init commit

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Made changes to master");
+        System.out.println("Made changes to master...");
     }
 }
