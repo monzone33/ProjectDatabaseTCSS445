64a1d8946d45c12c5590b09a6de873272e6d7e2d
tempest138 <andrewmoreno355@gmail.com>
2022-06-03 23:48:10 -0700

    changes

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Change to master from branch");
+        System.out.println("my new branch");
     }
 }
