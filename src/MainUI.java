d64c5000a234b1c67c429deb279d468a14bc6054
Emanuel Monzon <emanuel-monzon97@outlook.com>
2022-06-05 00:51:29 -0700

    Testing a table insertion.

--- src/MainUI.java
+++ src/MainUI.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.sql.*;
@@ -18,12 +19,14 @@ public class MainUI {
         myFrame.setSize(800, 600);
 
         panel = new JPanel();
-        panel.setBounds(0, 0, 800, 600);
-        panel.setBackground(Color.BLACK);
+        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+        JPanel buttons = new JPanel();
+        JScrollPane tablePanel = new JScrollPane();
         JButton basicsButton = new JButton("Basics");
         JButton loreButton = new JButton("Lore");
         JButton itemsButton = new JButton("Items");
         JButton runesButton = new JButton("Runes");
+        tablePanel.getViewport().add(addInitialTable());
 
         basicsButton.setBounds(10, 20, 100, 20);
         basicsButton.setBackground(Color.GREEN);
@@ -33,25 +36,51 @@ public class MainUI {
         itemsButton.setBackground(Color.GREEN);
         runesButton.setBounds(10, 140, 100, 20);
         runesButton.setBackground(Color.GREEN);
-        panel.setLayout(null);
-        panel.add(basicsButton);
-        panel.add(loreButton);
-        panel.add(itemsButton);
-        panel.add(runesButton);
-        addInitialTable();
+        buttons.add(basicsButton);
+        buttons.add(loreButton);
+        buttons.add(itemsButton);
+        buttons.add(runesButton);
+
+        panel.add(tablePanel);
         myFrame.add(panel);
         myFrame.setLocationRelativeTo(null);        // Positions Frame in Center.
-        myFrame.setResizable(false);
+        myFrame.setResizable(true);
         myFrame.setVisible(true);
     }
 
-    private void addInitialTable() throws SQLException {
+    private JTable addInitialTable() throws SQLException {
+        String columns[] = {"Name", "HP", "HP+", "HPREGEN", "HPREGEN+", "MANA", "MANA+", "MANAREGEN", "MANAREGEN+",
+                            "AD", "AD+", "AS", "AS+", "ARMOR", "ARMOR+", "MR", "MR+", "MS", "RANGE"};
+        String[][] championRows = new String[159][159];
+        String query = "SELECT * FROM CHAMPIONSTATS";
+        Statement st = connection.createStatement();
+        ResultSet result = st.executeQuery(query);
+        int row = 0;
+        while (result.next()) {
+            championRows[row][0] = result.getString(1);
+            championRows[row][1] = "" + result.getInt(2);
+            championRows[row][2] = "" + result.getInt(3);
+            championRows[row][3] = "" + result.getDouble(4);
+            championRows[row][4] = "" + result.getDouble(5);
+            championRows[row][5] = "" + result.getDouble(6);
+            championRows[row][6] = "" + result.getDouble(7);
+            championRows[row][7] = "" + result.getDouble(8);
+            championRows[row][8] = "" + result.getDouble(9);
+            championRows[row][9] = "" + result.getInt(10);
+            championRows[row][10] = "" + result.getDouble(11);
+            championRows[row][11] = "" + result.getDouble(12);
+            championRows[row][12] = "" + result.getDouble(13);
+            championRows[row][13] = "" + result.getInt(14);
+            championRows[row][14] = "" + result.getDouble(15);
+            championRows[row][15] = "" + result.getDouble(16);
+            championRows[row][16] = "" + result.getDouble(17);
+            championRows[row][17] = "" + result.getDouble(18);
+            championRows[row][18] = "" + result.getInt(19);
+            row++;
+        }
+        return new JTable(championRows, columns);
     }
 
-    /**
-     * Returns a connection to the database.
-     * @return
-     */
     public Connection getConnection() {
         String url = "jdbc:mysql://localhost:3306/project";
         String user = "root";
